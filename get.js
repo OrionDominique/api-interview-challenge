7ae820d70ebfe47ed28e7c5b2cb437fd2b17301e
Daniel <ds.sikor@gmail.com>
2019-07-20 09:50:58 +0200

    Added Get, Push + Request Framework

---
+++ get.js
@@ -0,0 +1,21 @@
+var request = require("request");
+
+var options = { method: 'GET',
+  url: 'https://api-interview-challenge.poweredbyjourney.com/line_items/',
+  headers: 
+   { 'cache-control': 'no-cache',
+     Connection: 'keep-alive',
+     'accept-encoding': 'gzip, deflate',
+     Host: 'api-interview-challenge.poweredbyjourney.com',
+     'Postman-Token': 'a89bdc03-b2d1-411f-99f6-5d7158729d53,181a8bba-e8b2-4182-9a1f-0dceec2ce111',
+     'Cache-Control': 'no-cache',
+     Accept: '*/*',
+     'User-Agent': 'PostmanRuntime/7.15.0',
+     Authorization: 'Bearer glup3jjvzxkp1r33pipwld',
+     'Content-Type': 'application/json' } };
+
+request(options, function (error, response, body) {
+  if (error) throw new Error(error);
+
+  console.log(body);
+});
---
+++ post.js
@@ -0,0 +1,54 @@
+var request = require("request");
+
+var options = { method: 'POST',
+  url: 'https://api-interview-challenge.poweredbyjourney.com/purchase_order/',
+  headers: 
+   { 'Postman-Token': 'cc0bfbe5-9895-4475-9cf1-56e5f028d75f',
+     'cache-control': 'no-cache',
+     Authorization: 'Bearer glup3jjvzxkp1r33pipwld',
+     Submission: 'Daniel3',
+     'Content-Type': 'application/json' },
+  body: 
+   { po4vlhht: 
+      [ { lineItemId: 'ypvzh',
+          stockCode: '3333',
+          packSize: '5 kg',
+          quantity: 38,
+          lotNumbers: 
+           [ { lotNumber: '32217',
+               quantity: 38,
+               expiryDate: '2021-02-24T00:00:00.000Z' } ] },
+        { lineItemId: '0lpaf',
+          stockCode: '3333',
+          packSize: '2.5 kg',
+          quantity: 9,
+          lotNumbers: 
+           [ { lotNumber: '33182',
+               quantity: 9,
+               expiryDate: '2020-06-20T00:00:00.000Z' } ] },
+        { lineItemId: '1gv6u',
+          stockCode: '5555',
+          packSize: '10 kg',
+          quantity: 8,
+          lotNumbers: 
+           [ { lotNumber: '15478',
+               quantity: 8,
+               expiryDate: '2020-01-22T00:00:00.000Z' } ] },
+        { lineItemId: 'vapwg',
+          stockCode: '5555',
+          packSize: '5 kg',
+          quantity: 82,
+          lotNumbers: 
+           [ { lotNumber: '14106',
+               quantity: 29,
+               expiryDate: '2020-04-28T00:00:00.000Z' },
+             { lotNumber: '57365',
+               quantity: 53,
+               expiryDate: '2020-12-09T00:00:00.000Z' } ] } ] },
+  json: true };
+
+request(options, function (error, response, body) {
+  if (error) throw new Error(error);
+
+  console.log(body);
+});
